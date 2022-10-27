6b8add0599481dd89602e9189cf159f605172552
chemi <wbsldj7645@naver.com>
2022-10-27 22:50:01 +0900

    [ADD] Calendar 추가

---
+++ DailyReport/Global/Extension/UIFont+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIFont+Extension.swift
+//  DailyReport
+//
+//  Created by Mingwan Choi on 2022/10/27.
+//
+
+import Foundation
--- DailyReport/Screens/Home/HomeViewController.swift
+++ DailyReport/Screens/Home/HomeViewController.swift
@@ -7,6 +7,34 @@
 
 import UIKit
 
+import FSCalendar
+import SnapKit
+
 final class HomeViewController: BaseViewController {
     
+    // MARK: - property
+    
+    private let homeCalendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.today = Date()
+        calendar.appearance.titleFont = .label2
+        calendar.locale = Locale(identifier: "ko_KR")
+        calendar.appearance.headerDateFormat = "YYYY년 MM월"
+        calendar.appearance.headerTitleColor = .black
+        calendar.appearance.headerTitleFont = .main
+        calendar.appearance.headerMinimumDissolvedAlpha = 0.0
+        calendar.appearance.weekdayFont = .label2
+        return calendar
+    }()
+    
+    // MARK: - life cycle
+    
+    override func render() {
+        view.addSubview(homeCalendar)
+        homeCalendar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(300)
+        }
+    }
 }
