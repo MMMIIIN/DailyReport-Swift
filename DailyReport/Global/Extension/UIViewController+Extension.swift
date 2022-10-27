8558c6a312769b65e4bb2132db059140c9c5521a
chemi <wbsldj7645@naver.com>
2022-10-27 20:44:59 +0900

    [SETTING] project setting

---
+++ DailyReport/Global/Extension/UIViewController+Extension.swift
@@ -0,0 +1,83 @@
+//
+//  UIViewController+Extension.swift
+//  DailyReport
+//
+//  Created by Mingwan Choi on 2022/10/27.
+//
+
+import UIKit
+
+extension UIViewController {
+    func makeAlert(title: String,
+                   message: String? = nil,
+                   okAction: ((UIAlertAction) -> Void)? = nil,
+                   completion : (() -> Void)? = nil) {
+        let alertViewController = UIAlertController(title: title,
+                                                    message: message,
+                                                    preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        
+        self.present(alertViewController, animated: true, completion: completion)
+    }
+    
+    func makeRequestAlert(title: String,
+                          message: String,
+                          okTitle: String = "확인",
+                          cancelTitle: String = "취소",
+                          okAction: ((UIAlertAction) -> Void)?,
+                          cancelAction: ((UIAlertAction) -> Void)? = nil,
+                          completion : (() -> Void)? = nil) {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: cancelTitle, style: .default, handler: cancelAction)
+        alertViewController.addAction(cancelAction)
+        
+        let okAction = UIAlertAction(title: okTitle, style: .destructive, handler: okAction)
+        alertViewController.addAction(okAction)
+        
+        self.present(alertViewController, animated: true, completion: completion)
+    }
+    
+    func makeActionSheet(title: String? = nil,
+                         message: String? = nil,
+                         actionTitles:[String?],
+                         actionStyle:[UIAlertAction.Style],
+                         actions:[((UIAlertAction) -> Void)?]) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        
+        for (index, title) in actionTitles.enumerated() {
+            let action = UIAlertAction(title: title, style: actionStyle[index], handler: actions[index])
+            alert.addAction(action)
+        }
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(endEditingView))
+        tap.cancelsTouchesInView = false
+        tap.delegate = self
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func endEditingView() {
+        view.endEditing(true)
+    }
+}
+
+extension UIViewController: UIGestureRecognizerDelegate {
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard let count = self.navigationController?.viewControllers.count else { return false }
+        return count > 1
+    }
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+         guard touch.view is UIButton else { return true }
+         return false
+     }
+}
---
+++ DailyReport/Screens/Home/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  DailyReport
+//
+//  Created by Mingwan Choi on 2022/10/27.
+//
+
+import UIKit
+
+final class HomeViewController: BaseViewController {
+    
+}
